b9529b5b0a73a7971036c43485a552043f30e56b
Jeffrey Clark <dude@zaplabs.com>
2012-07-27 14:49:10 -0500

    make build script work anywhere in the repo

--- build.sh
+++ build.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
+SCRIPT_PATH=$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 COMMAND="$1"
 ADDITIONAL="$2"
+
+# Support functions
+
+function cleanup {
+    # End timer and print elapsed time
+    if [ ! -z "$START" ]; then
+        END=$(date +%s)
+        ELAPSED=$((END - START))
+        E_MIN=$((ELAPSED / 60))
+        E_SEC=$((ELAPSED - E_MIN * 60))
+        printf "Elapsed: "
+        [ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+        printf "%d sec(s)\n " $E_SEC
+    fi
+
+    popd >/dev/null
+    exit
+}
+
+function findup {
+    local locatefile=$1
+    local currentpath=$2
+    local match=''
+    [ -z "$currentpath" ] && currentpath=$(pwd)
+    match=$(find "${currentpath}/" -maxdepth 1 -name "${locatefile}" 2>/dev/null)
+    if [ ! -z "$match" ]; then
+        echo $match
+    else
+        if [[ "${currentpath}" != "/" ]]; then
+            findup "${locatefile}" "$(dirname "${currentpath}")"
+        fi
+    fi
+}
+
+function die {
+    echo -e $* >&2
+    exit 1
+}
+
+trap cleanup INT TERM EXIT
+
+REPO_PATH=`dirname $(findup '.repo') 2>/dev/null`
+[ -z "${REPO_PATH}" ] && die "Unable to find repo"
+pushd "${REPO_PATH}" >/dev/null
 
 # Starting timer
 START=$(date +%s)
@@ -10,41 +56,25 @@ START=$(date +%s)
     case "$COMMAND" in
         clean)
                 make clobber
-                exit
                 ;;
         prepare)
                 repo sync -j8
-                exit
                 ;;
 
-        skyrocket)
-                ./hercskytools/cm10-${COMMAND}-apply.sh
+        skyrocket | hercules | quincyatt)
+                if [ -f "${SCRIPT_PATH}/cm10-${COMMAND}-apply.sh" ]; then
+                    ${SCRIPT_PATH}/cm10-${COMMAND}-apply.sh
+                fi
                 . build/envsetup.sh
                 ./vendor/cm/get-prebuilts
-                brunch cm_skyrocket-eng
-                ;;
-        hercules)
-                ./hercskytools/cm10-${COMMAND}-apply.sh
-                . build/envsetup.sh
-                ./vendor/cm/get-prebuilts
-                brunch cm_hercules-eng
+                brunch cm_${COMMAND}-eng
                 ;;
         *)
+                unset START
                 echo -e "Usage: $0 DEVICE ADDITIONAL"
                 echo -e "Example: ./build.sh skyrocket"
-                echo -e "Supported Devices: skyrocket, hercules"
                 echo -e "Use: ./build.sh clean to make clobber"
                 echo -e "Use: ./build.sh prepare to repo sync"
                 exit 2
                  ;;
 esac
-
-# End timer and print elapsed time
-END=$(date +%s)
-ELAPSED=$((END - START))
-E_MIN=$((ELAPSED / 60))
-E_SEC=$((ELAPSED - E_MIN * 60))
-printf "Elapsed: "
-[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
-printf "%d sec(s)\n " $E_SEC
-
